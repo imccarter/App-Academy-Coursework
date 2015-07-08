d6576b0229da933ce3613eb375003a103a9babc6
Ivan McCarter <ivanmccarter@gmail.com>
2015-07-08 13:51:00 -0700

    Initial commit, ShortenedUrl and User migrations and model classes

---
+++ app/models/shortened_url.rb
@@ -0,0 +1,21 @@
+class ShortenedUrl < ActiveRecord::Base
+
+  validates :short_url, presence: true, uniqueness: true
+  validates :long_url, presence: true
+
+  def self.random_code
+    short_url = SecureRandom::urlsafe_base64
+    while self.exists?(short_url: short_url)
+      short_url = SecureRandom::urlsafe_base64
+    end
+    short_url
+  end
+
+  def self.create_for_user_and_long_url!(user, long_url)
+    ShortenedUrl.create!(short_url: ShortenedUrl.random_code,
+      submitter_id: user.id,
+      long_url: long_url
+    )
+  end
+
+end
