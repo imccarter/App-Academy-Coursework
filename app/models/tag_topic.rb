c00be7acd4f006ccad8c5ab3a2dc007dc1ebe365
Ivan McCarter <ivanmccarter@gmail.com>
2015-07-08 18:08:42 -0700

    Work on tag_topc#most_popular_urls, fix tagging/tag_topic associations

--- app/models/tag_topic.rb
+++ app/models/tag_topic.rb
@@ -14,16 +14,7 @@ class TagTopic < ActiveRecord::Base
     source: :url
   )
 
-  # def most_popular_urls(num)
-  #   self.urls.order(num_clicks)
-  #
-  #   SELECT
-  #     *
-  #   FROM
-  #     urls
-  #   ORDER BY
-  #     num_clicks
-  #   LIMIT
-  #     num
-  # end
+  def most_popular_urls(num)#Untested...
+    self.urls.joins(:visits).group("urls.id").order("COUNT(visits.*)").limit(num)
+  end
 end
