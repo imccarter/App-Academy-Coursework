6ca2412d8aa74844f0dffe8f217b2d0c540d9045
App Academy Student <student@appacademy.io>
2015-07-22 16:31:45 -0700

    Commit for revisit of project; got belongs_to to work

---
+++ lib/00_attr_accessor_object.rb
@@ -0,0 +1,18 @@
+class AttrAccessorObject
+  def self.my_attr_accessor(*names)
+
+
+    names.each do |name|
+      #getter
+      define_method(name) do
+        instance_variable_get('@' + name.to_s)
+      end
+
+      #setter
+      define_method((name.to_s + '=')) do |value|
+        instance_variable_set(('@' + name.to_s), value)
+      end
+
+    end
+  end
+end
